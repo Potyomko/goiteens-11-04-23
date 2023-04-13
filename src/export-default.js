207da608b06baea8cd5ad53b171c4067235a7608
Potyomko Veronika <vnpotomko@gmail.com>
2023-04-13 18:19:59 +0300

    11.04.23

---
+++ src/export-default.js
@@ -0,0 +1,8 @@
+const user = {
+    name: 'userName',
+    age: 15,
+    getAge(){
+        console.log(this.age);
+    }
+}
+export default user
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import {getAge} from './export-default';
+
+console.log(getAge());
+
+const coordsOutputRef = document.querySelector('.js-coords');
+let mouseMoveCbInvocationCounter = 0;
+
+window.addEventListener('mousemove', debounce(onMouseMove, 250) );
+function onMouseMove(event) {
+  mouseMoveCbInvocationCounter += 1;
+
+  coordsOutputRef.textContent = `
+    Кількість викликів onMouseMove: ${mouseMoveCbInvocationCounter},
+    X: ${event.clientX},
+    Y:${event.clientY}
+  `;
+}
+/*
+ * Input и debounce
+ */
+const inputRef = document.querySelector('.js-input');
+const outputRef = document.querySelector('.js-output');
+let inputCbInvocationCounter = 0;
+
+inputRef.addEventListener('input', throttle(onInputChange, 2500));
+function onInputChange(event) {
+  inputCbInvocationCounter += 1;
+
+  outputRef.textContent = `
+    Кількість викликів onInputChange: ${inputCbInvocationCounter},
+    Значення: ${event.target.value}
+  `;
+}
